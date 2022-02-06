fbcaad9ad3a3a98755f85560035e6292d77cbc8c
AleksandraTodorovich <aleksandratodorovic84@gmail.com>
2022-02-06 14:14:47 +0100

    Domaci

---
+++ src/d_03_02_2022/Zadatak2.java
@@ -0,0 +1,15 @@
+package d_03_02_2022;
+
+public class Zadatak2 {
+
+	public static void main(String[] args) {
+//		2.Zadatak
+//		Napisati program koji:
+//		Ucitava stanicu https://www.wikipedia.org/
+//		Sa stranice sakuplja sve linkove (jezike) i svaki link otvara u novom prozoru pretrazivaca
+//		Svaki link potrebno je otvoriti u novom tabu.
+//		Skripta: window.open(arguments[0]);
+
+	}
+
+}
